a28953ae7918cec8e9c2924a0798eb756b3ad5a8
tutorwellinton-gif <tutorwellinton@gmail.com>
2025-10-02 18:17:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+meme_dict = {
+            "CRINGE": "Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Investigar a vida de alguém online",
+            }
 
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    # O que devemos fazer se a palavra for encontrada?
+    print(meme_dict[word])
+else:
+    # O que devemos fazer se a palavra não for encontrada?
+    print("Palavra não encontrada")
